6df547873e94bef931fcae096aa5bc8f72d4de54
boomSx <117458879+boomSx@users.noreply.github.com>
2024-08-24 13:27:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}`);
+});
 import {
   Client,
   GatewayIntentBits,
